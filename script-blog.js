eec7a30fa184b1632f27b1caba3dd7a1673ed66d
iplmr <45397969+iplmr@users.noreply.github.com>
2023-11-16 09:53:10 +0300

    HW

--- script-blog.js
+++ script-blog.js
@@ -2,23 +2,21 @@
 CKEDITOR.replace("textarea");
 let blog = document.getElementById("blog");
 let btn = document.getElementById("btn");
-let deleteBtn = document.getElementById("delete-button");
+let clearBtn = document.getElementById("clear-button");
 let userEmail = document.getElementById("username");
 
 let storedUserEmail = localStorage.getItem("User");
 
 if (storedUserEmail !== null && storedUserEmail !== undefined) {
-    userEmail.innerHTML = storedUserEmail;
-}
-else{
-    userEmail.innerHTML = "Gest";
-
+  userEmail.innerHTML = storedUserEmail;
+} else {
+  userEmail.innerHTML = "Gest";
 }
 btn.addEventListener("click", () => {
   let texterea = CKEDITOR.instances.textarea.getData();
   blog.insertAdjacentHTML("beforeend", texterea);
 });
 
-deleteBtn.addEventListener("click", () => {
+clearBtn.addEventListener("click", () => {
   blog.innerHTML = "";
 });
